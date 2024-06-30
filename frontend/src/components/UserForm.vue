4186b130ae9eac40bf1bd32c9cf9ee4a81c93d02
VncsSz <84037924+VncsSz@users.noreply.github.com>
2024-06-29 22:47:42 -0300

    Conn back and front

--- frontend/src/components/UserForm.vue
+++ frontend/src/components/UserForm.vue
@@ -52,7 +52,52 @@ export default {
 
             e.preventDefault();
 
-            console.log("OK")
+            const data = {
+                name: this.name,
+                email: this.email,
+                password: this.password,
+                confirmpassword: this.confirmpassword
+            }
+
+            const jsonData = JSON.stringify(data)
+
+            
+            await fetch("http://localhost:3000/api/auth/register",{
+                method: "POST",
+                headers:{"Content-type": "application/json"},
+                body: jsonData
+            })
+            .then((resp) => resp.json())
+            .then((data) =>{
+                
+                let auth = false;
+                
+                if(data.error){
+                    this.msg = data.error;
+                    this.msgClass = "error";
+                } else{
+
+                    auth = true;
+
+                    this.msg = data.msg;
+                    this.msgClass = "success";
+
+                    //Emit event for auth an user
+                }
+
+                setTimeout(() =>{
+                    if(!auth){
+                        this.msg = null;
+                    } else {
+                        //Redirect
+                        this.$router.push("dashboard")
+                    }
+
+                }, 2000)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
         }
     }
 
