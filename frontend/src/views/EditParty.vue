21a693eb63b022c706588a450be8d3c70146bb4d
VncsSz <84037924+VncsSz@users.noreply.github.com>
2024-09-07 16:15:40 -0300

    EditParty view

---
+++ frontend/src/views/EditParty.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="editparty">
+        <h1>Edite sua Festa</h1>
+        <PartyForm :party="party" page="editparty" btnText="Editar Festa" :key="componentKey"/>
+    </div>
+</template>
+
+<script>
+import PartyForm from '../components/PartyForm.vue';
+
+export default {
+    components:{
+        PartyForm
+    },
+    data() {
+        return {
+            party: {},
+            componentKey: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+.editparty{
+    text-align: center;
+    padding-top: 40px;
+    padding-bottom: 100px;
+}
+
+.editparty h1{
+    margin-bottom: 40px;
+}
+</style>
