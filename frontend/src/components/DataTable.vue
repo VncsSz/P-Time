8fcfaa54a764fafca17db63c63f0f4250b8b4021
VncsSz <84037924+VncsSz@users.noreply.github.com>
2024-09-07 13:16:43 -0300

    Remove function and css

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-import { RouterLink } from "vue-router";
 import Message from "./Message.vue";
 
 export default {
@@ -36,11 +35,100 @@ export default {
         msg: null,
         msgClass: null
         }
+    },
+    methods: {
+        async remove(id) {
+            //Get id and token from store
+            const userId = this.$store.getters.userId;
+            const token = this.$store.getters.token;
+
+            const data = {
+                id: id,
+                userId: userId
+            };
+
+            const jsonData = JSON.stringify(data);
+
+            await fetch("http://localhost:3000/api/party", {
+                method: "DELETE",
+                headers: {
+                    "Content-type": "application/json",
+                    "auth-token": token
+                },
+                body:jsonData
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+
+                if(data.error) {
+                    this.msg = data.error;
+                    this.msgClass = "error";
+                } else {
+                    this.msg = data.msg;
+                    this.msgClass = "success";
+                }
+
+                setTimeout (() => {
+                    
+                    this.msg = null;
+
+                    //Get all parties again
+                    this.$parent.getParties();
+                }, 1000);
+            })
+            .catch((err) =>{
+                console.log(err);
+            })
+        }
     }
+
 
 }
 </script>
 
 <style scoped>
+
+.data-table-heading, .data-row {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    border-bottom: 1px solid #CCC;
+}
+
+.data-table-heading div, .data-row div {
+    flex: 1 1 0;
+}
+
+.data-id-heading, .data-id-container {
+    max-width: 50px;
+}
+
+.edit-btn, .remove-btn {
+    padding: 10px 16px;
+    background-color: #000;
+    color: #FFF;
+    margin: 5px;
+    text-decoration: none;
+    border: none;
+    font-size: 14px;
+    cursor: pointer;
+    transition: .5s;
+}
+
+.edit-btn {
+    background-color: #007BFF;
+}
+
+.edit-btn:hover {
+    background-color: #0069D9;
+}
+
+.remove-btn {
+    background-color: #DC3545;
+}
+
+.remove-btn:hover {
+    background-color: #C82333;
+}
 
 </style>
